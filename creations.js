41d5cad40cb799a5c8e5ce676b0032c96fd0dcbb
ren <minakei160@gmail.com>
2026-03-16 00:19:42 -0300

    f

--- creations.js
+++ creations.js
@@ -2,19 +2,19 @@ var creationsData = [
     {
         "date": "03.09.2026",
         "title": "Kokona & Saki",
-        "image": "/img/ren-KokonaSakiRender.png",
+        "image": "./img/ren-KokonaSakiRender.png",
         "description": ""
     },
     {
         "date": "02.15.2026",
         "title": "GC Render",
-        "image": "/img/ren-GCrender.png",
+        "image": "./img/ren-GCrender.png",
         "description": ""
     },
     {
         "date": "12.25.2025",
         "title": "Christmas Render",
-        "image": "/img/christmasRender_01.png",
+        "image": "./img/christmasRender_01.png",
         "description": ""
     }
 ];
