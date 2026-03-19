f65daea8a55b832b3e6d930785596fd61e78e085
ren <minakei160@gmail.com>
2026-03-19 17:18:44 -0300

    Update creations.js

--- creations.js
+++ creations.js
@@ -1,4 +1,5 @@
-var creationsData = [
+// 1. Art Gallery Data (your renders!)
+var artData = [
     {
         "date": "03.09.2026",
         "title": "Kokona & Saki",
@@ -17,4 +18,26 @@ var creationsData = [
         "image": "/img/creations/christmasRender_01.png",
         "description": ""
     }
+];
+
+// 2. Human Universe OCs
+var ocsHumanData = [
+    /* template for when you are ready to add them:
+    {
+        "title": "character name",
+        "image": "/img/creations/human-oc.png",
+        "description": "all of their lore and details go right here!"
+    }
+    */
+];
+
+// 3. Fantasy Universe OCs
+var ocsFantasyData = [
+    /* template for when you are ready to add them:
+    {
+        "title": "character name",
+        "image": "/img/creations/fantasy-oc.png",
+        "description": "all of their lore and details go right here!"
+    }
+    */
 ];
