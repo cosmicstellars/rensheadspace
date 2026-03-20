832bf2e0295a077a97476ccb27947a0c7606be24
ren <minakei160@gmail.com>
2026-03-20 01:17:58 -0300

    big update

--- ocs.js
+++ ocs.js
@@ -1,22 +1,64 @@
 var ocsHumanData = [
     {
-        "id": "seiichi",  // optional, but ensures the link works perfectly!
+        "id": "seiichi",  
         "title": "Seiichi",
         "image": "/img/ocs/seiichi/icon.png",
-        // "category": "Main Cast",
+		"wip": false,
         
-        // new stats!
         "age": "21",
         "gender": "male",
         "pronouns": "he/him",
         "sexuality": "homosexual",
         
-        // new lore!
-        "description": "just a regular guy navigating the city. he loves video games and drawing.",
+        "description": "no description yet",
         "backstory": "no backstory yet",
-        // "relationships": "best friends with <a href='/character.html?char=bob'>bob</a>. avoids jim at all costs.",
         
+        "relationships": [
+            {
+                "category": "family",
+                "chars": ["mizuki"] // placeholder for when you make her!
+            },
+            {
+                // "category": "friends",
+                // "chars": ["bob"] // this will automatically link to bob below!
+            }
+        ],
+        
+        "gallery": [
+            {
+                "image": "/img/ocs/seiichi/gallery/render.png",   
+            },
+            {
+                "image": "/img/ocs/seiichi/gallery/veemodraw.png" 
+                "description": "drawing by veemo"
+            }
+        ]
+    }, // <-- notice the comma here! this separates the characters.
+     {
+        "id": "Mizuki",
+        "title": "Mizuki",
+        "image": "/img/ocs/mizuki/icon.png"
+		"wip": true,
+        
+        "age": "",
+        "gender": "",
+        "pronouns": ",
+        "sexuality": "",
+        
+        "description": "",
+        "backstory": "",
+        
+        "relationships": [
+            {
+                "category": "family",
+                "chars": ["seiichi"] // links right back to seiichi!
+            }
+        ],
+        
+        "gallery": [] // you can leave it empty if he has no art yet!
     }
 ];
 
-// ... your fantasy array down here
+var ocsFantasyData = [
+    // your magic/fantasy characters will go in here, formatted the exact same way!
+];
