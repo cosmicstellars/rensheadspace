787d443b4d3e8ea838d0b42da7a53720846f625e
ren <minakei160@gmail.com>
2026-03-20 01:01:15 -0300

    seiichi

--- ocs.js
+++ ocs.js
@@ -1,23 +1,21 @@
 var ocsHumanData = [
     {
-        "id": "ren",  // optional, but ensures the link works perfectly!
-        "title": "ren",
-        "image": "/img/creations/human-oc1.png",
-        "category": "Main Cast",
+        "id": "seiichi",  // optional, but ensures the link works perfectly!
+        "title": "Seiichi",
+        "image": "/img/ocs/seiichi/icon.png",
+        // "category": "Main Cast",
         
         // new stats!
-        "age": "19",
+        "age": "21",
         "gender": "male",
         "pronouns": "he/him",
-        "sexuality": "bisexual",
+        "sexuality": "homosexual",
         
         // new lore!
         "description": "just a regular guy navigating the city. he loves video games and drawing.",
-        "backstory": "born in a quiet town, he moved to the big city to pursue his dreams of creating the ultimate interactive experience.",
-        "relationships": "best friends with <a href='/character.html?char=bob'>bob</a>. avoids jim at all costs.",
+        "backstory": "no backstory yet",
+        // "relationships": "best friends with <a href='/character.html?char=bob'>bob</a>. avoids jim at all costs.",
         
-        // where their custom gallery lives
-        "gallery": "/gallery-ren.html"
     }
 ];
 
