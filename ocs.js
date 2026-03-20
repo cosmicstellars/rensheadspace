713da1fe4e6dd429ee65b0987c339e36d1d2fbe7
ren <minakei160@gmail.com>
2026-03-20 00:32:19 -0300

    ff

---
+++ ocs.js
@@ -0,0 +1,33 @@
+// file: ocs.js
+
+var ocsHumanData = [
+    {
+        "title": "ren",
+        "image": "/img/creations/human-oc1.png",
+        "description": "just a regular guy.",
+        "category": "Main Cast" // this creates a header!
+    },
+    {
+        "title": "background bob",
+        "image": "/img/creations/human-oc2.png",
+        "description": "he has no category, so he will float at the top."
+        // leaving category blank is totally fine
+    }
+];
+
+var ocsFantasyData = [
+    {
+        "title": "elara",
+        "image": "/img/creations/fantasy-oc1.png",
+        "description": "she wields the power of the stars.",
+        "category": "The Rebellion",
+        "species": "Star Elf" // this will show up under her name!
+    },
+    {
+        "title": "grom",
+        "image": "/img/creations/fantasy-oc2.png",
+        "description": "he likes rocks.",
+        "category": "Villains",
+        "species": "Goblin"
+    }
+];
