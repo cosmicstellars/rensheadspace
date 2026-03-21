86d8f028e87ec95e834b1a024b876e4c6154c4d4
ren <minakei160@gmail.com>
2026-03-21 15:24:45 -0300

    update

--- creations.js
+++ creations.js
@@ -1,21 +1,51 @@
-// 1. Art Gallery Data (your renders!)
 var artData = [
     {
-        "date": "03.09.2026",
-        "title": "Kokona & Saki",
+        "title": "Getting ready with Itachi",
+        "date": "02.03.2026",
+        "image": "/img/creations/ren-ItachiZametora.png",
+        "description": "I'm gonna post renders from time to time of Sports Club content from my AU :D<br><br><br><br>Looks like Itachi's getting bothered by his fellow club members while he's the last to change for practice...<br><br><br><br>Credits:<br><br>GAS, David, PrincessDie, QVA, Noice, SizeableMovie, Miyusakii, LimeyHarkov"
+    },
+    {
+        "title": "Itachi Zametora's Casual",
+        "date": "02.03.2026",
+        "image": "/img/creations/ren-ItachiCasual.png",
+        "description": "I plan on rendering the Sports Club outside of school, so I'm working on a few casual outfits for them!<br><br>Since Itachi's model is already done, here is his outfit! I'll still give it some tweaks later on <br><br><br><br>Credits: LimeyHarkov, Miyusakii, PrincessDie, David, GAS"
+    },
+    {
+        "title": "Itachi Close-up",
+        "date": "02.04.2026",
+        "image": "/img/creations/ren-ItachiCloseUp.png",
+        "description": "I decided to update a few things in his textures, by giving him some freckles! I think they do look pretty nice on him :)<br><br>If you're wondering where did the favorites from my last posts go, it's because I reposted them. Next up the character I want to get done is Unagi! <br><br>Credits: LimeyHarkov, Miyusakii, David, Noice, PrincessDie, GAS"
+    },
+    {
+        "title": "Unagi Denkashiza Redesign",
+        "date": "02.05.2026",
+        "image": "/img/creations/ren-UnagiCloseUp.png",
+        "description": "Here's Unagi Denkashiza! All credit goes to saiyanro on Tumblr for his design :) Fun fact, I've had this hair model of his done for a while now and decided that it would be best if I just retextured it and I won't lie, it looks 10x better!<br><br>His casual outfit will be modelled soon, hope you're looking foward to that!! ;3<br><br>Credits: Saiyanro, David, PrincessDie, Miyusakii, QVA, GAS"
+    },
+    {
+        "title": "Unagi Denkashiza Casual",
+        "date": "02.07.2026",
+        "image": "/img/creations/ren-UnagiCasual.png",
+        "description": "Here's Unagi's casual! Honestly, I might still edit some things here and there but I'm uploading this at 4am in my timezone so I need to sleep lol<br><br><br><br>Credits: Saiyanro, PrincessDie, Miyusakii, ATLUS, GAS"
+    },
+    {
+        "title": "Pranked by Friends...?",
+        "date": "02.09.2026",
+        "image": "/img/creations/ren-UnagiRender.png",
+        "description": "While out in another city with his friends, a strange man came up to Unagi proposing money to him in exchange of something. Unagi thought this was weird and though his friends were pranking him. His friends claimed that they had no connection with this man nor had idea who he was.<br><br>Will Unagi take the money...?<br><br>Credits: Saiyanro, PrincessDie, Miyusakii, ATLUS, GAS, NoicePotato"
+    },
+    {
+        "title": "Kokona & Saki (Download)",
+        "date": "03.05.2026",
         "image": "/img/creations/ren-KokonaSakiRender.png",
-        //"description": ""
+        "description": "I wanted to make some hair edits with the GameAssetStudio models for quite a while now, so I made hair edits for Kokona and Saki! :)<br><br>They're up for grabs, so you can download them! Just credit me and GameAssetStudio when using and don't redistribute!<br>If you're curious about my AU, I'll post stuff again about it soo, I just took a small break to work on other stuff!!<br>Credits: GameAssetStudio, Me"
     },
     {
-        "date": "02.15.2026",
-        "title": "GC Render",
+        "title": "#ONENIGHT OCs",
+        "date": "03.02.2026",
         "image": "/img/creations/ren-GCrender.png",
-        //"description": ""
-    },
-    {
-        "date": "12.25.2025",
-        "title": "Christmas Render",
-        "image": "/img/creations/christmasRender_01.png",
-        //"description": ""
+        "description": "I choose you everytime ♡<br><br>Made a render of me and my friends' OCs together!! Was very fun to do the editing :-)<br>Credits: Miyusakii, K4I, PrincessDie, Neko, Koala, Ophelia, GameAssetStudio"
     }
 ];
+
